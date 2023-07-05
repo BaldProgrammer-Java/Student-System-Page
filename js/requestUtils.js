cb5bc942ae7a2737b3a9d015281c251c36d1a1c0
baldprogrammer <3149693028@qq.com>
2023-07-06 01:36:03 +0800

    backup

---
+++ js/requestUtils.js
@@ -0,0 +1,16 @@
+/**
+ *  配置请求头类型
+ * @type {{headers: {"Content-Type": string}}}
+ */
+const headersConfig = {
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+};
+
+/**
+ *  创建FormData(Form表单)对象
+ * @type {FormData}
+ */
+const formData = new FormData();
+
